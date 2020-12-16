aebf6a174e4929b54af82061fa0ffd4ebb1b82c2
Tobi <tobiasanzinger98@gmail.com>
2020-12-16 11:41:32 +0100

    add GiroKontoTest

---
+++ src/test/java/aufgabeBankkonten/GiroKontoTest.java
@@ -0,0 +1,47 @@
+package aufgabeBankkonten;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@RunWith(MockitoJUnitRunner.class)
+public class GiroKontoTest {
+
+    @Mock
+    Kunde kundeMock;
+
+    GiroKonto giroKonto;
+
+    double amount1 = 120.34;
+    double amount2 = -120.34;
+    double amount3 = 0;
+
+    @Before
+    public void setUp(){
+        giroKonto = new GiroKonto(kundeMock, 1);
+        giroKonto.setSaldo(1000);
+    }
+
+
+    @Test
+    public void einzahlenTest() {
+        assertTrue(giroKonto.einzahlen(amount1));
+        assertFalse(giroKonto.einzahlen(amount2));
+        assertFalse(giroKonto.einzahlen(amount3));
+        assertEquals(1000 + amount1, giroKonto.getSaldo());
+    }
+
+    @Test
+    public void abhebenTest() {
+        assertTrue(giroKonto.abheben(amount1));
+        assertEquals(1000 - amount1, giroKonto.getSaldo());
+        assertFalse(giroKonto.abheben(amount2));
+        assertEquals(1000 - amount1, giroKonto.getSaldo());
+        assertFalse(giroKonto.abheben(amount3));
+        assertEquals(1000 - amount1 - amount3, giroKonto.getSaldo());
+    }
+}
