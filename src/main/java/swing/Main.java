1243a187275fa87cc97d1fc5368caaad812957a1
Tobi <tobiasanzinger98@gmail.com>
2021-05-04 14:43:34 +0200

    add SwingDisplay

--- src/main/java/swing/Main.java
+++ src/main/java/swing/Main.java
@@ -2,14 +2,16 @@ package swing;
 
 
 import javax.sound.sampled.*;
+import java.io.IOException;
 
 public class Main extends Thread {
     static Clip clip;
 
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException, UnsupportedAudioFileException, LineUnavailableException, IOException {
 
 //        new EasyWindow2();
+//        new EasyWindow();
         for (int i = 0; i < 30; i++) {
             Thread t = new T();
             t.start();
---
+++ src/main/java/swingDisplay/Pixel.java
@@ -0,0 +1,62 @@
+package swingDisplay;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class Pixel extends JFrame {
+
+    int pixelWidth;
+    int pixelHeight;
+    int pos_x;
+    int pos_y;
+    boolean visible;
+
+
+    public Pixel(DPSettings dpSettings, Pos pos, boolean visible) {
+        super();
+        pixelWidth = dpSettings.screenSize.width / dpSettings.pixel_x;
+        pixelHeight = dpSettings.screenSize.height / dpSettings.pixel_y;
+        this.pos_x = pos.x * pixelWidth;
+        this.pos_y = pos.y * pixelHeight;
+        this.visible = visible;
+
+        setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
+        final Container con = getContentPane();
+        con.setLayout(new BorderLayout());
+
+        setSize(pixelWidth, pixelHeight);
+        setLocation(pos_x, pos_y);
+        setVisible(this.visible);
+    }
+
+    @Override
+    public String toString() {
+        return "Pixel{" +
+                "pixelWidth=" + pixelWidth +
+                ", pixelHeight=" + pixelHeight +
+                ", pos_x=" + pos_x +
+                ", pos_y=" + pos_y +
+                ", visible=" + visible +
+                '}';
+    }
+}
+
+class Pos {
+    int x;
+    int y;
+    Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+
+    public Pos(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    @Override
+    public String toString() {
+        return "Loc{" +
+                "x=" + x +
+                ", y=" + y +
+                ", screenSize=" + screenSize +
+                '}';
+    }
+}
