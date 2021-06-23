073f777a75bbb3583e642d6f15b2053524b51875
Tobias Anzinger <Tobias.Anzinger@jambit.com>
2021-06-23 13:45:48 +0200

    add MyListModel

---
+++ src/main/java/todoList/MyListModel.java
@@ -0,0 +1,218 @@
+package todoList;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+
+import javax.swing.*;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.util.*;
+import java.util.function.Consumer;
+import java.util.function.Predicate;
+import java.util.function.UnaryOperator;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
+public class MyListModel<E> extends AbstractListModel<E> implements ListModel<E> {
+
+    private ArrayList<E> delegate = new ArrayList<>();
+
+
+    @Override
+    public int getSize() {
+        return 0;
+    }
+
+    @Override
+    public E getElementAt(int index) {
+        return null;
+    }
+
+    public void trimToSize() {
+        delegate.trimToSize();
+    }
+
+    public void ensureCapacity(int minCapacity) {
+        delegate.ensureCapacity(minCapacity);
+    }
+
+    public int size() {
+        return delegate.size();
+    }
+
+    public boolean isEmpty() {
+        return delegate.isEmpty();
+    }
+
+    public boolean contains(Object o) {
+        return delegate.contains(o);
+    }
+
+    public int indexOf(Object o) {
+        return delegate.indexOf(o);
+    }
+
+    public int lastIndexOf(Object o) {
+        return delegate.lastIndexOf(o);
+    }
+
+    public Object[] toArray() {
+        return delegate.toArray();
+    }
+
+    public <T> T[] toArray(T[] a) {
+        return delegate.toArray(a);
+    }
+
+    public E get(int index) {
+        return delegate.get(index);
+    }
+
+    public E set(int index, E element) {
+        return delegate.set(index, element);
+    }
+
+    public boolean add(E e) {
+        return delegate.add(e);
+    }
+
+    public void add(int index, E element) {
+        delegate.add(index, element);
+    }
+
+    public E remove(int index) {
+        return delegate.remove(index);
+    }
+
+    public boolean remove(Object o) {
+        return delegate.remove(o);
+    }
+
+    public void clear() {
+        delegate.clear();
+    }
+
+    public boolean addAll(Collection<? extends E> c) {
+        return delegate.addAll(c);
+    }
+
+    public boolean addAll(int index, Collection<? extends E> c) {
+        return delegate.addAll(index, c);
+    }
+
+    public boolean removeAll(Collection<?> c) {
+        return delegate.removeAll(c);
+    }
+
+    public boolean retainAll(Collection<?> c) {
+        return delegate.retainAll(c);
+    }
+
+    public ListIterator<E> listIterator(int index) {
+        return delegate.listIterator(index);
+    }
+
+    public ListIterator<E> listIterator() {
+        return delegate.listIterator();
+    }
+
+    public Iterator<E> iterator() {
+        return delegate.iterator();
+    }
+
+    public List<E> subList(int fromIndex, int toIndex) {
+        return delegate.subList(fromIndex, toIndex);
+    }
+
+    public void forEach(Consumer<? super E> action) {
+        delegate.forEach(action);
+    }
+
+    public Spliterator<E> spliterator() {
+        return delegate.spliterator();
+    }
+
+    public boolean removeIf(Predicate<? super E> filter) {
+        return delegate.removeIf(filter);
+    }
+
+    public void replaceAll(UnaryOperator<E> operator) {
+        delegate.replaceAll(operator);
+    }
+
+    public void sort(Comparator<? super E> c) {
+        delegate.sort(c);
+    }
+
+    public boolean containsAll(Collection<?> c) {
+        return delegate.containsAll(c);
+    }
+
+    public Stream<E> stream() {
+        return delegate.stream();
+    }
+
+    public Stream<E> parallelStream() {
+        return delegate.parallelStream();
+    }
+
+
+    static final String path = "src/main/java/todoList/savefile.json";
+
+    public static boolean save(JList<TodoList.Entry> list) {
+
+        JSONObject obj = new JSONObject();
+        MyListModel<TodoList.Entry> myListModel = (MyListModel<TodoList.Entry>) list.getModel();
+        JSONArray jsonArray = new JSONArray();
+        List<TodoList.Entry> l = IntStream.range(0, myListModel.getSize()).mapToObj(myListModel::getElementAt)
+                .collect(Collectors.toList());
+        jsonArray.addAll(l);
+
+        obj.put("list", jsonArray);
+
+        try {
+            FileWriter fw = new FileWriter(path);
+            fw.write(obj.toJSONString());
+            fw.flush();
+            fw.close();
+            return true;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
+
+    }
+
+    public static MyListModel<TodoList.Entry> read() throws Exception {
+        String f = readFile();
+        System.out.println(f);
+        JSONParser parser = new JSONParser();
+        JSONObject jsonObject = new JSONObject((Map) parser.parse(f));
+        JSONArray jsonArray = (JSONArray) jsonObject.get("list");
+        MyListModel<TodoList.Entry> lm = new MyListModel<>();
+        for (Object o : jsonArray) {
+            lm.add((TodoList.Entry) o);
+        }
+        return lm;
+    }
+
+    private static String readFile() {
+        try {
+            FileReader fr = new FileReader(path);
+            BufferedReader br = new BufferedReader(fr);
+            StringBuilder sb = new StringBuilder();
+            java.lang.String line;
+            while ((line = br.readLine()) != null) {
+                sb.append(line);
+            }
+            return sb.toString();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+}
